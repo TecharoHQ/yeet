acc84adc45c052f3b7a0d28322bce2346c3dea3a
Xe Iaso <me@xeiaso.net>
2025-03-29 10:25:08 -0400

    add spooky scary program skeleton
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- yeetfile.js
+++ yeetfile.js
@@ -1,15 +1,13 @@
-go.install();
-
-["amd64", "arm64"].forEach(goarch => {
+["amd64", "arm64"].forEach(goarch =>
     [deb, rpm].forEach(method => method.build({
         name: "yeet",
         description: "Yeet out scripts with maximum haste!",
-        homepage: "https://within.website",
+        homepage: "https://techaro.lol",
         license: "CC0",
         goarch,
 
         build: (out) => {
-            go.build("-o", `${out}/usr/bin/yeet`);
+            go.build("-o", `${out}/usr/bin/yeet`, "./cmd/yeet");
         },
-    }));
-});
+    }))
+);

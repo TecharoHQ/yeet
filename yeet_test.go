cd60a2ee19f0878049687df3a458a1fe1cfb8d8b
Xe Iaso <me@xeiaso.net>
2025-04-26 18:31:17 -0400

    test: make packages get built to a temporary directory
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- yeet_test.go
+++ yeet_test.go
@@ -1,6 +1,7 @@
 package yeet
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
@@ -13,7 +14,21 @@ func TestBuildOwnPackages(t *testing.T) {
 		t.Skip("Skipping test in non-CI environment")
 	}
 
+	type packageJSON struct {
+		Version string `json:"version"`
+	}
+
+	fin, err := os.ReadFile("package.json")
+	if err != nil {
+		t.Fatalf("can't read package.json: %v", err)
+	}
+
+	var pkg packageJSON
+	if err := json.Unmarshal(fin, &pkg); err != nil {
+		t.Fatalf("can't unmarshal package.json: %v", err)
+	}
+
 	dir := t.TempDir()
 	internal.PackageDestDir = &dir
-	yeet.ShouldWork(t.Context(), nil, yeet.WD, "go", "run", "./cmd/yeet")
+	yeet.ShouldWork(t.Context(), nil, yeet.WD, "go", "run", "./cmd/yeet", "--force-git-version", pkg.Version, "--package-dest-dir", t.TempDir())
 }

848fb49e8f1cd6fc366f1bd2a9c3afc9d48ae27b
Xe Iaso <me@xeiaso.net>
2026-07-10 13:08:55 -0400

    test(mkportable): test all build methods
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- internal/mkportable/mkportable_test.go
+++ internal/mkportable/mkportable_test.go
@@ -9,31 +9,6 @@ import (
 	"github.com/TecharoHQ/yeet/internal/yeettest"
 )
 
-func TestBuild(t *testing.T) {
-	const method = "test"
-
-	myBuild := func(p pkgmeta.Package) (string, error) {
-		return build(p, method)
-	}
-
-	keyFname := filepath.Join(t.TempDir(), "foo.gpg")
-	keyID, err := gpgtest.MakeKey(t.Context(), keyFname)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fname := yeettest.BuildHello(t, myBuild, yeettest.BuildHelloInput{
-		Version:  "1.0.0",
-		KeyFname: keyFname,
-		KeyID:    keyID,
-		Fatal:    true,
-
-		GOOS:   "linux",
-		GOARCH: "amd64",
-	})
-	t.Log(fname)
-}
-
 func TestBuildError(t *testing.T) {
 	const method = "test"
 
@@ -45,4 +20,34 @@ func TestBuildError(t *testing.T) {
 		Version: ".0.0",
 		Fatal:   false,
 	})
+}
+
+func TestAllMethods(t *testing.T) {
+	keyFname := filepath.Join(t.TempDir(), "foo.gpg")
+	keyID, err := gpgtest.MakeKey(t.Context(), keyFname)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range []struct {
+		name    string
+		builder func(pkgmeta.Package) (string, error)
+	}{
+		{"confext", Confext},
+		{"portable", Portable},
+		{"sysext", Sysext},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			fname := yeettest.BuildHello(t, tt.builder, yeettest.BuildHelloInput{
+				Version:  "1.0.0",
+				KeyFname: keyFname,
+				KeyID:    keyID,
+				Fatal:    true,
+
+				GOOS:   "linux",
+				GOARCH: "amd64",
+			})
+			t.Log(fname)
+		})
+	}
 }
